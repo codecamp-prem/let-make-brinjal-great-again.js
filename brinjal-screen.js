190f3a1812e9040361930c048d18465a80f81bc3
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2018-08-27 02:05:12 +0530

    Update brinjal-screen.js

--- brinjal-screen.js
+++ brinjal-screen.js
@@ -9,3 +9,20 @@
   el.style.top = ((Math.random() * (innerHeight + pageYOffset)) | 0) + `px`;
   document.body.appendChild(el);
 })();
+
+
+// More with rotation
+
+(function r() {
+  setTimeout(r, Math.random() * 10);
+  let el = document.createElement(`div`);
+  el.innerHTML = `🍆`;
+  el.style.position = `absolute`;
+  el.style.zIndex = 999999;
+  el.style.transform = `rotate(${Math.random() * 360}deg)`;
+  el.style.opacity = 0.3 + Math.random();
+  el.style.fontSize = (((Math.random() * 48) | 0) + 16) + `px`;
+  el.style.left = ((Math.random() * innerWidth) | 0) + `px`;
+  el.style.top = ((Math.random() * (innerHeight + pageYOffset)) | 0) + `px`;
+  document.body.appendChild(el);
+})();
